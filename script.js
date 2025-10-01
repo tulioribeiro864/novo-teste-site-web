9fe512f26adf4645a1de11fc6011567ae804f624
tulioribeiro864 <trn@aluno.ifnmg.edu.br>
2025-10-01 20:34:21 +0000

    css

--- script.js
+++ script.js
@@ -122,34 +122,3 @@ if (logoutBtn) {
   });
   }
   
-// Seleciona os elementos
-const heroTextTitle = document.querySelectorAll(".hero_text_title li span");
-
-// Cria uma timeline do GSAP
-let heroSectionTL = gsap.timeline();
-
-// Animação do título
-heroSectionTL.from(heroTextTitle, {
-  duration: 1.2,
-  opacity: 0,
-  y: 100,
-  ease: "power4.out",
-  stagger: 0.2
-});
-
-// Animação dos botões
-heroSectionTL.from(".btn", {
-  duration: 1,
-  opacity: 0,
-  scale: 0.5,
-  ease: "back.out(1.7)",
-  stagger: 0.3
-}, "-=0.8");
-
-// Animação da imagem
-heroSectionTL.from(".hero_image img", {
-  duration: 1.5,
-  opacity: 0,
-  scale: 0.7,
-  ease: "elastic.out(1, 0.5)"
-}, "-=1");
