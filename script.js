6f6898b862883e0788c0e17ad5a7ab1d7dbd3547
tulioribeiro864 <trn@aluno.ifnmg.edu.br>
2025-10-08 00:21:01 +0000

    et

--- script.js
+++ script.js
@@ -185,3 +185,14 @@ if (navToggle && sidebar) {
     }
   });
 }
+document.addEventListener("DOMContentLoaded", () => {
+  const toggle = document.querySelector(".menu-toggle img");
+  const menu = document.querySelector(".menu");
+
+  toggle.addEventListener("click", () => {
+    menu.classList.toggle("show");
+    toggle.style.transform = menu.classList.contains("show")
+      ? "rotate(180deg)"
+      : "rotate(0deg)";
+  });
+});
